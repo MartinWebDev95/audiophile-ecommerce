80b61893d7747db54df3498e4652332aad2678a5
MartinWebDev95 <jamll.muro@gmail.com>
2023-01-09 09:39:45 +0100

    Add initial config

---
+++ src/AudiophileEcommerce.jsx
@@ -0,0 +1,11 @@
+import './AudiophileEcommerce.css';
+
+function AudiophileEcommerce() {
+  return (
+    <div className="App">
+      Hi world!
+    </div>
+  );
+}
+
+export default AudiophileEcommerce;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AudiophileEcommerce from './AudiophileEcommerce';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AudiophileEcommerce />
+  </React.StrictMode>,
+);
