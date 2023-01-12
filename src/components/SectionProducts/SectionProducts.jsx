3cfeddd5bed16ff18d8294bc456bfbc12b9ba97d
MartinWebDev95 <jamll.muro@gmail.com>
2023-01-12 12:03:31 +0100

    Add new component SectionProducts

---
+++ src/components/SectionProducts/SectionProducts.jsx
@@ -0,0 +1,66 @@
+import { Link } from 'react-router-dom';
+import styles from './SectionProducts.module.css';
+
+function SectionProducts() {
+  return (
+    <section className={styles.sectionProducts}>
+      <div className={styles.containerFirstSpeaker}>
+        <div className={styles.speaker}>
+          <picture>
+            <source media="(min-width: 1000px)" srcSet="./images/home/desktop/image-speaker-zx9.png" />
+            <source media="(min-width: 480px)" srcSet="./images/home/tablet/image-speaker-zx9.png" />
+            <img src="./images/home/mobile/image-speaker-zx9.png" alt="ZX9 Speaker" />
+          </picture>
+          <div className={styles.speakerInfo}>
+            <h2>ZX9 Speaker</h2>
+            <p>
+              Upgrade to premium speakers that are phenomenally
+              built to deliver truly remarkable sound.
+            </p>
+            <div className={styles.speakerLink}>
+              <Link to="/">
+                See Product
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.containerSecondSpeaker}>
+        <div className={styles.secondSpeaker}>
+          <picture>
+            <source media="(min-width: 1000px)" srcSet="./images/home/desktop/image-speaker-zx7.jpg" />
+            <source media="(min-width: 480px)" srcSet="./images/home/tablet/image-speaker-zx7.jpg" />
+            <img src="./images/home/mobile/image-speaker-zx7.jpg" alt="ZX7 Speaker" />
+          </picture>
+          <div className={styles.secondSpeakerInfo}>
+            <h2>ZX7 Speaker</h2>
+            <div className={styles.secondSpeakerLink}>
+              <Link to="/">
+                See Product
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.containerEarphones}>
+        <div className={styles.earphonesImage}>
+          <picture>
+            <source media="(min-width: 1000px)" srcSet="./images/home/desktop/image-earphones-yx1.jpg" />
+            <source media="(min-width: 480px)" srcSet="./images/home/tablet/image-earphones-yx1.jpg" />
+            <img src="./images/home/mobile/image-earphones-yx1.jpg" alt="YX1 Earphones" />
+          </picture>
+        </div>
+        <div className={styles.earphonesInfo}>
+          <h2>YX1 Earphones</h2>
+          <div className={styles.earphonesLink}>
+            <Link to="/">
+              See Product
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default SectionProducts;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Header from '../../components/Header/Header';
 import HeroSection from '../../components/HeroSection/HeroSection';
 import ListCategories from '../../components/ListCategories/ListCategories';
+import SectionProducts from '../../components/SectionProducts/SectionProducts';
 import styles from './Home.module.css';
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
         <section className={styles.sectionCategories}>
           <ListCategories />
         </section>
+        <SectionProducts />
       </main>
     </>
   );
