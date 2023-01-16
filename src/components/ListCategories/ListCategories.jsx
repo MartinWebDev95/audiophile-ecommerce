1a0f5e4db1d0e34c61b17cc8126736ce5bc927af
MartinWebDev95 <jamll.muro@gmail.com>
2023-01-16 13:56:33 +0100

    Change Link for NavLink in ListCategories component to have the active link in a different color

--- src/components/ListCategories/ListCategories.jsx
+++ src/components/ListCategories/ListCategories.jsx
@@ -1,51 +1,65 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './ListCategories.module.css';
 
 function ListCategories({ type = '' }) {
   return (
     <ul className={styles.navigationList} type={type}>
       <li className={styles.navigationItem} type={type}>
-        <Link to="/">
-          <span className={styles.itemTitle} type={type}>
-            Home
-          </span>
-        </Link>
+        <NavLink to="/">
+          {({ isActive }) => (
+            <span className={`${styles.itemTitle} ${isActive ? styles.linkActive : ''}`} type={type}>
+              Home
+            </span>
+          )}
+        </NavLink>
       </li>
       <li className={styles.navigationItem} type={type}>
-        <Link to="/headphones">
-          <img src="./images/shared/desktop/image-headphones.png" alt="" />
-          <span className={styles.itemTitle} type={type}>
-            Headphones
-          </span>
-          <span className={styles.itemShop} type={type}>
-            Shop
-            <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M1.322 1l5 5-5 5" stroke="#D87D4A" strokeWidth="2" fill="none" fillRule="evenodd" /></svg>
-          </span>
-        </Link>
+        <NavLink to="/headphones">
+          {({ isActive }) => (
+            <>
+              <img src="./images/shared/desktop/image-headphones.png" alt="" />
+              <span className={`${styles.itemTitle} ${isActive ? styles.linkActive : ''}`} type={type}>
+                Headphones
+              </span>
+              <span className={styles.itemShop} type={type}>
+                Shop
+                <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M1.322 1l5 5-5 5" stroke="#D87D4A" strokeWidth="2" fill="none" fillRule="evenodd" /></svg>
+              </span>
+            </>
+          )}
+        </NavLink>
       </li>
       <li className={styles.navigationItem} type={type}>
-        <Link to="/speakers">
-          <img src="./images/shared/desktop/image-speakers.png" alt="" />
-          <span className={styles.itemTitle} type={type}>
-            Speakers
-          </span>
-          <span className={styles.itemShop} type={type}>
-            Shop
-            <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M1.322 1l5 5-5 5" stroke="#D87D4A" strokeWidth="2" fill="none" fillRule="evenodd" /></svg>
-          </span>
-        </Link>
+        <NavLink to="/speakers">
+          {({ isActive }) => (
+            <>
+              <img src="./images/shared/desktop/image-speakers.png" alt="" />
+              <span className={`${styles.itemTitle} ${isActive ? styles.linkActive : ''}`} type={type}>
+                Speakers
+              </span>
+              <span className={styles.itemShop} type={type}>
+                Shop
+                <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M1.322 1l5 5-5 5" stroke="#D87D4A" strokeWidth="2" fill="none" fillRule="evenodd" /></svg>
+              </span>
+            </>
+          )}
+        </NavLink>
       </li>
       <li className={styles.navigationItem} type={type}>
-        <Link to="/earphones">
-          <img src="./images/shared/desktop/image-earphones.png" alt="" />
-          <span className={styles.itemTitle} type={type}>
-            Earphones
-          </span>
-          <span className={styles.itemShop} type={type}>
-            Shop
-            <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M1.322 1l5 5-5 5" stroke="#D87D4A" strokeWidth="2" fill="none" fillRule="evenodd" /></svg>
-          </span>
-        </Link>
+        <NavLink to="/earphones">
+          {({ isActive }) => (
+            <>
+              <img src="./images/shared/desktop/image-earphones.png" alt="" />
+              <span className={`${styles.itemTitle} ${isActive ? styles.linkActive : ''}`} type={type}>
+                Earphones
+              </span>
+              <span className={styles.itemShop} type={type}>
+                Shop
+                <svg width="8" height="12" xmlns="http://www.w3.org/2000/svg"><path d="M1.322 1l5 5-5 5" stroke="#D87D4A" strokeWidth="2" fill="none" fillRule="evenodd" /></svg>
+              </span>
+            </>
+          )}
+        </NavLink>
       </li>
     </ul>
   );
