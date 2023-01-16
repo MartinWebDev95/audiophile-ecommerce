a1d4f6ed507194272d368a8dd8ea06ebdaca3154
MartinWebDev95 <jamll.muro@gmail.com>
2023-01-16 11:27:41 +0100

    Add content to Headphones page

--- src/pages/Headphones/Headphones.jsx
+++ src/pages/Headphones/Headphones.jsx
@@ -1,6 +1,24 @@
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import ListCategories from '../../components/ListCategories/ListCategories';
+import ListProducts from '../../components/ListProducts/ListProducts';
+import SectionInfo from '../../components/SectionInfo/SectionInfo';
+import styles from './Headphones.module.css';
+
 function Headphones() {
   return (
-    <div>Headphones</div>
+    <>
+      <Header />
+      <main className={styles.main}>
+        <h1 className={styles.title}>Headphones</h1>
+        <ListProducts />
+        <section className={styles.sectionCategories}>
+          <ListCategories />
+        </section>
+        <SectionInfo />
+      </main>
+      <Footer />
+    </>
   );
 }
 
