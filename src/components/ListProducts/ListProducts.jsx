eae0294d38acaf7bf9a3d947b2bf2a9956f334e7
MartinWebDev95 <jamll.muro@gmail.com>
2023-01-16 11:36:46 +0100

    Remove some styles of ListProducts component

--- src/components/ListProducts/ListProducts.jsx
+++ src/components/ListProducts/ListProducts.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import data from '../../products.json';
+import Product from '../Product/Product';
 import styles from './ListProducts.module.css';
 
 function ListProducts() {
@@ -13,27 +13,7 @@ function ListProducts() {
   return (
     <section className={styles.sectionListProducts}>
       {products.map((product) => (
-        <div className={styles.containerProduct} key={product.id}>
-          <picture>
-            <source media="(min-width: 1000px)" srcSet={product.categoryImage.desktop} />
-            <source media="(min-width: 480px)" srcSet={product.categoryImage.tablet} />
-            <img srcSet={product.categoryImage.mobile} alt={product.shortName} />
-          </picture>
-          <div>
-            {product.new && (<p className={styles.newProduct}>New Product</p>)}
-            <h2>
-              {product.name}
-            </h2>
-            <p className={styles.productDescription}>
-              {product.description}
-            </p>
-            <div className={styles.productLink}>
-              <Link to="/">
-                See Product
-              </Link>
-            </div>
-          </div>
-        </div>
+        <Product key={product.id} product={product} />
       ))}
     </section>
   );
