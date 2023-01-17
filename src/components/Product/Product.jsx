55baeb91cc16e642f19f089340c6f2db7badfffb
MartinWebDev95 <jamll.muro@gmail.com>
2023-01-17 11:36:04 +0100

    Change the Product component to make it reusable in ProductDetailPage too

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,29 +1,79 @@
 import { Link } from 'react-router-dom';
 import styles from './Product.module.css';
 
-function Product({ product }) {
+function Product({ product = {}, detail = false }) {
   return (
-    <div className={styles.containerProduct} key={product.id}>
-      <picture>
-        <source media="(min-width: 1000px)" srcSet={product.categoryImage.desktop} />
-        <source media="(min-width: 480px)" srcSet={product.categoryImage.tablet} />
-        <img srcSet={product.categoryImage.mobile} alt={product.shortName} />
-      </picture>
-      <div>
-        {product.new && (<p className={styles.newProduct}>New Product</p>)}
-        <h2>
-          {product.name}
-        </h2>
-        <p className={styles.productDescription}>
-          {product.description}
-        </p>
-        <div className={styles.productLink}>
-          <Link to={product.slug}>
-            See Product
-          </Link>
+    !detail
+      ? (
+        <div className={styles.containerProduct}>
+          <picture>
+            <source media="(min-width: 1000px)" srcSet={product.categoryImage.desktop} />
+            <source media="(min-width: 480px)" srcSet={product.categoryImage.tablet} />
+            <img srcSet={product.categoryImage.mobile} alt={product.shortName} />
+          </picture>
+
+          <div>
+            {product.new && (<p className={styles.newProduct}>New Product</p>)}
+
+            <h2>
+              {product.name}
+            </h2>
+
+            <p className={styles.productDescription}>
+              {product.description}
+            </p>
+
+            <div className={styles.productLink}>
+              <Link to={product.slug}>
+                See Product
+              </Link>
+            </div>
+          </div>
+
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className={styles.containerProductDetail}>
+          <picture>
+            <source media="(min-width: 1000px)" srcSet={product.image.desktop} />
+            <source media="(min-width: 480px)" srcSet={product.image.tablet} />
+            <img srcSet={product.image.mobile} alt={product.shortName} />
+          </picture>
+
+          <div>
+            {product.new && (<p className={styles.newProductDetail}>New Product</p>)}
+
+            <h2>
+              {product.name}
+            </h2>
+
+            <p className={styles.productDetailDescription}>
+              {product.description}
+            </p>
+
+            <p className={styles.productDetailPrice}>
+              {`$ ${product.price.toLocaleString('en-US')}`}
+            </p>
+
+            <div className={styles.productDetailCart}>
+              <div className={styles.containerQuantityInput}>
+                <button type="button">
+                  +
+                </button>
+
+                <input type="number" id="quantity" name="quantity" placeholder="1" />
+
+                <button type="button">
+                  -
+                </button>
+              </div>
+
+              <button type="button" className={styles.buttonAddToCart}>
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      )
   );
 }
 
