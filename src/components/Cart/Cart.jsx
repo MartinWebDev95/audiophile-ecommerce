26b2664256e8f5789effb3d47dee0f38f30849c3
Juan Antonio <jamll.muro@gmail.com>
2023-02-07 12:54:26 +0100

    Change border radius of quantity input to 0

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -34,57 +34,58 @@ function Cart({ openCart, setOpenCart }) {
       onClick={handleClick}
     >
       <div className={styles.cartContainer}>
-        {cart.products?.length === 0 ? (
-          <div className={styles.cartContainerEmpty}>
-            <h2 className={styles.cartTitleEmpty}>Your cart is empty</h2>
-            <img className={styles.cartImageEmpty} src="/images/cart/empty-cart.png" alt="Empty Cart" loading="lazy" />
-          </div>
-        ) : (
-          <>
-            <section className={styles.cartHeader}>
-              <p className={styles.quantityCart}>
-                Cart (
-                {`${cart.products?.length}`}
-                )
-              </p>
-
-              <button
-                type="button"
-                className={styles.cartRemoveAll}
-                onClick={handleRemoveAll}
-              >
-                Remove all
-              </button>
-            </section>
-
-            <section className={styles.containerCartList}>
-              <ul className={styles.listCartItem}>
-                {cart.products?.map((item) => (
-                  <ProductCart
-                    key={item.id}
-                    item={item}
-                  />
-                ))}
-              </ul>
-            </section>
-
-            <section className={styles.containerCheckout}>
-              <div className={styles.cartTotalPrice}>
-                <p>Total:</p>
-                <p>
-                  {`$ ${cart?.totalPriceCart.toLocaleString('en-US')}`}
+        {cart.products?.length === 0
+          ? (
+            <div className={styles.cartContainerEmpty}>
+              <h2 className={styles.cartTitleEmpty}>Your cart is empty</h2>
+              <img className={styles.cartImageEmpty} src="/images/cart/empty-cart.png" alt="Empty Cart" loading="lazy" />
+            </div>
+          ) : (
+            <>
+              <section className={styles.cartHeader}>
+                <p className={styles.quantityCart}>
+                  Cart (
+                  {`${cart.products?.length}`}
+                  )
                 </p>
-              </div>
 
-              <Link
-                to="/checkout"
-                className={styles.linkCheckout}
-              >
-                Checkout
-              </Link>
-            </section>
-          </>
-        )}
+                <button
+                  type="button"
+                  className={styles.cartRemoveAll}
+                  onClick={handleRemoveAll}
+                >
+                  Remove all
+                </button>
+              </section>
+
+              <section className={styles.containerCartList}>
+                <ul className={styles.listCartItem}>
+                  {cart.products?.map((item) => (
+                    <ProductCart
+                      key={item.id}
+                      item={item}
+                    />
+                  ))}
+                </ul>
+              </section>
+
+              <section className={styles.containerCheckout}>
+                <div className={styles.cartTotalPrice}>
+                  <p>Total:</p>
+                  <p>
+                    {`$ ${cart?.totalPriceCart.toLocaleString('en-US')}`}
+                  </p>
+                </div>
+
+                <Link
+                  to="/checkout"
+                  className={styles.linkCheckout}
+                >
+                  Checkout
+                </Link>
+              </section>
+            </>
+          )}
       </div>
     </div>
   );
